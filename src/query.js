daf5bbd4062c83ffab882570ef9563754f692f00
yago.ferrer <yago.ferrer@servicenow.com>
2019-12-10 11:57:20 -0800

    all users

--- src/query.js
+++ src/query.js
@@ -1,7 +1,10 @@
 const query = {
     me(parent, args, {db}, info) {
        return db.users.find((user) => user.id ==='1')
-    }
+    },
+    allUsers(parent, args, {db}, info) {
+        return db.users
+    },
 }
 
 export default query
